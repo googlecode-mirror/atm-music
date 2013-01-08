caf8530f36a9b4e6513555018b2437e9eaa1271e
bastien.thouverez <bastien.thouverez@3e335893-a9d2-7b6a-605f-b2e89115ca72>
2013-01-08 00:22:25 +0000

    image

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -1,6 +1,7 @@
 <?php echo render('partial.head'); ?>
-
-<p id="accueilP">BIENVENUE SUR L'APPLICATION <br>DE GESTION DE PLAYLIST</p>
+<div id="accueilDiv">
+<?php echo HTML::image('img/ATM.png', 'logo ATM'); ?>
+<p id="accueilP">BIENVENUE SUR L'APPLICATION <br>DE GESTION DE PLAYLIST ATM</p>
 <?php ?>
-
+</div>
 <?php echo render('partial.foot'); ?>
--- application/views/partial/head.php
+++ application/views/partial/head.php
@@ -3,8 +3,9 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-        <title>Appli Péon : Oppan Péon Style !</title>
+        <title>At The Moment - Music</title>
         <meta name="viewport" content="width=device-width">
+        <link rel="icon" type="image/png" href="../img/icone.png" />
         <?php echo HTML::style('/css/font.css') ?>
         <?php echo HTML::style('/css/head.css') ?>
         <?php echo HTML::style('/css/content.css') ?>
