af1f0c24d5fc1f59a9f36618affae2e916314ad5
bastien.thouverez <bastien.thouverez@3e335893-a9d2-7b6a-605f-b2e89115ca72>
2013-01-13 17:05:17 +0000

    Last commit, je savais que ce serait moi !!!!!

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <title></title>
+		<meta http-equiv="refresh" content="0;URL=public">
     </head>
     <body>
         <?php
